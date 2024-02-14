06f2c532c167b869a1045b3a909d747694cb8726
Titouan <titouan.brando@etu.iut-tlse3.fr>
2024-02-14 15:05:29 +0100

    Fin TP3

--- index.php
+++ index.php
@@ -9,6 +9,10 @@ switch ($http_method){
     break;
   case 'POST' :
     $data = recupererDataDansCorps();
+    if (!isset($data['phrase'])) {
+      deliver_response(400,'[R401 API REST] : Phrase manquante (paramètre ou valeur à vérifier)');
+      die();
+    }
     if ($data = insert($data['phrase'])) {
       deliver_response(200, 'OK',$data);
     } else {
@@ -16,33 +20,17 @@ switch ($http_method){
     }
     break;
   case 'PATCH': 
-    if(isset($_GET['id']))
-    {
-      $id=htmlspecialchars($_GET['id']);
-      $data = recupererDataDansCorps();
-      $phrase = getData($id)[0];
-      if (empty($phrase)) {
-        deliver_response(404, 'Not found');
-      }
-      else {
-        foreach ($phrase as $key => $value) {
-          if (isset($data[$key])) {
-            $phrase[$key] = $data[$key];
-          }
-        }
-        update($phrase);
-        deliver_response(200,'OK',$phrase);
-      }
-    }
-    else {
-      deliver_response(400, '[R401 API REST] : paramètre id manquant');
-    }
+    traiterPatch();
     break;
   case 'PUT':
     if(isset($_GET['id']))
     {
       $id=htmlspecialchars($_GET['id']);
       $data = recupererDataDansCorps();
+      if (!isset($data['phrase'])) {
+        deliver_response(400,'[R401 API REST] : Phrase manquante (paramètre ou valeur à vérifier)');
+        die();
+      }
       $phrase = getData($id)[0];
       if (empty($phrase)) {
         deliver_response(404, 'Not found');
@@ -101,7 +89,6 @@ function traiterGet() {
       }
       else {
         $data = getData();
-
       }
     }
     else {
@@ -113,6 +100,7 @@ function traiterGet() {
             $data = getLast();
           }
           break;
+
         case 'popular':
           if (!empty($_GET['n'])) {
             $data = getPopular($_GET['n']);
@@ -120,15 +108,102 @@ function traiterGet() {
             $data = getPopular();
           }
           break;
+
         case 'reported':
           if (!empty($_GET['n'])) {
             $data = getReported($_GET['n']);
           } else {
             $data = getReported();
           }
-      }
+        default:
+          deliver_response(404, 'Ressource incoonue ou Mauvaise méthode HTTP');
+          die();
     }
     deliver_response(200, 'OK', $data);
+  }
+}
+
+function traiterPatch() {
+  //Récupération des données dans l’URL
+  if(!isset($_GET['action'])) {
+    if(isset($_GET['id']))
+    {
+      $id=htmlspecialchars($_GET['id']);
+      $data = recupererDataDansCorps();
+      if (!isset($data['phrase'])) {
+        deliver_response(400,'[R401 API REST] : Phrase manquante (paramètre ou valeur à vérifier)');
+        die();
+      }
+      $phrase = getData($id)[0];
+      if (empty($phrase)) {
+        deliver_response(404, 'Not found');
+      }
+      else {
+        foreach ($phrase as $key => $value) {
+          if (isset($data[$key])) {
+            $phrase[$key] = $data[$key];
+          }
+        }
+        update($phrase);
+        deliver_response(200,'OK',$phrase);
+      }
+    }
+    else {
+      deliver_response(400, '[R401 API REST] : paramètre id manquant');
+    }
+  }
+  else {
+    switch($_GET['action']) {
+      case 'upvote':
+        if (!empty($_GET['n'])) {
+          $id=htmlspecialchars($_GET['n']);
+          $data = getData($id)[0];
+          $data['vote'] += 1;
+          update($data);
+        } else {
+          deliver_response(400, '[R401 API REST] : paramètre id manquant');
+          die();
+        }
+        break;
+      case 'downvote':
+        if (!empty($_GET['n'])) {
+          $id=htmlspecialchars($_GET['n']);
+          $data = getData($id)[0];
+          $data['vote'] -= 1;
+          update($data);
+        } else {
+          deliver_response(400, '[R401 API REST] : paramètre id manquant');
+          die();
+        }
+        break;
+        case 'report':
+          if (!empty($_GET['n'])) {
+            $id=htmlspecialchars($_GET['n']);
+            $data = getData($id)[0];
+            $data['signalement'] = 1;
+            update($data);
+          } else {
+            deliver_response(400, '[R401 API REST] : paramètre id manquant');
+            die();
+          }
+          break;
+        case 'unreport':
+          if (!empty($_GET['n'])) {
+            $id=htmlspecialchars($_GET['n']);
+            $data = getData($id)[0];
+            $data['signalement'] = 0;
+            update($data);
+          } else {
+            deliver_response(400, '[R401 API REST] : paramètre id manquant');
+            die();
+          }
+          break;
+    default:
+      deliver_response(404, 'Ressource incoonue ou Mauvaise méthode HTTP '.$_GET['action']);
+      die();
+    }
+  }
+  deliver_response(200,"Données id:$id modifiées avec succès",$data);
 }
 function recupererDataDansCorps() {
     // Récupération des données dans le corps
